9489f4868a12cc16bd8b748cce9d7fd6052d1e5c
Shubham Date <84425801+shubhamdate@users.noreply.github.com>
2022-05-20 15:30:58 +0530

    Delete AI and CC Lab/AI directory

--- AI and CC Lab/AI/Assignment 6/hospital_1.py
+++
@@ -1,6 +0,0 @@
-name = input("Enter is your name?")
-print("Welcome "+ name +". This Expert system which is created by Shubham Date to your help for the ")
-
-while True:
-    print("hello")
-    break
